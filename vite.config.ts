42af463537461dd9b264028283f1933f36ab1842
Nina Louise <ninalouise@reinina.moe>
2022-08-29 17:38:50 +0200

    Make the game installable as PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [
+		svelte(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			// devOptions: {
+			// 	enabled: true,
+			// },
+		}),
+	],
 	resolve: {
 		alias: {
 			src: resolve(__dirname, './src'),
